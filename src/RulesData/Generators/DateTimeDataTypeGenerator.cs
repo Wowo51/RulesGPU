cb9384d4a0e1ec11982f856986ca5145aa61f5f0
Warren Harding <warrencharding@yahoo.com>
2025-06-18 00:34:02 -0600

    Add src/RulesData/Generators/DateTimeDataTypeGenerator.cs

---
+++ src/RulesData/Generators/DateTimeDataTypeGenerator.cs
@@ -0,0 +1,22 @@
+//Copyright Warren Harding 2025.
+using System;
+
+namespace RulesData.Generators
+{
+    public class DateTimeDataTypeGenerator : IDataTypeGenerator
+    {
+        private readonly Random _random;
+
+        public DateTimeDataTypeGenerator(Random random)
+        {
+            _random = random;
+        }
+
+        public object Generate()
+        {
+            DateTime start = new DateTime(1990, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(_random.Next(range));
+        }
+    }
+}
