55da12aa4954f162e4f70930f35624308143c68f
Warren Harding <warrencharding@yahoo.com>
2025-06-18 00:34:13 -0600

    Add src/RulesDMN/Models/Decision.cs

---
+++ src/RulesDMN/Models/Decision.cs
@@ -0,0 +1,10 @@
+//Copyright Warren Harding 2025.
+namespace RulesDMN.Models
+{
+    public class Decision
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public IExpression? DecisionLogic { get; set; }
+    }
+}
