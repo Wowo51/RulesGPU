25d7ea74a17e7db29136c3a489c95b0080af2146
Warren Harding <warrencharding@yahoo.com>
2025-06-18 00:34:00 -0600

    Add src/RulesData/Generators/BooleanDataTypeGenerator.cs

---
+++ src/RulesData/Generators/BooleanDataTypeGenerator.cs
@@ -0,0 +1,20 @@
+//Copyright Warren Harding 2025.
+using System;
+
+namespace RulesData.Generators
+{
+    public class BooleanDataTypeGenerator : IDataTypeGenerator
+    {
+        private readonly Random _random;
+
+        public BooleanDataTypeGenerator(Random random)
+        {
+            _random = random;
+        }
+
+        public object Generate()
+        {
+            return _random.Next(2) == 0; // Random boolean (true/false)
+        }
+    }
+}
