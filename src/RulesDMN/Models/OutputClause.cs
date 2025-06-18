b38b1ef7b73470a2c7c52aaada5f7809611f473e
Warren Harding <warrencharding@yahoo.com>
2025-06-18 00:34:35 -0600

    Add src/RulesDMN/Models/OutputClause.cs

---
+++ src/RulesDMN/Models/OutputClause.cs
@@ -0,0 +1,10 @@
+//Copyright Warren Harding 2025.
+namespace RulesDMN.Models
+{
+    public class OutputClause
+    {
+        public string? Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string TypeRef { get; set; } = string.Empty;
+    }
+}
