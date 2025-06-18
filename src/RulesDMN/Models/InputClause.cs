5dc327bcd221a3711768df5bb9a0685859b20fea
Warren Harding <warrencharding@yahoo.com>
2025-06-18 00:34:27 -0600

    Add src/RulesDMN/Models/InputClause.cs

---
+++ src/RulesDMN/Models/InputClause.cs
@@ -0,0 +1,9 @@
+//Copyright Warren Harding 2025.
+namespace RulesDMN.Models
+{
+    public class InputClause
+    {
+        public string? Id { get; set; }
+        public LiteralExpression? Expression { get; set; }
+    }
+}
