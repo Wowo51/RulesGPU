6e9ec8d4bb243cd02b78587f170c8a4873001743
Warren Harding <warrencharding@yahoo.com>
2025-06-18 00:34:08 -0600

    Add src/RulesData/Generators/IntegerDataTypeGenerator.cs

---
+++ src/RulesData/Generators/IntegerDataTypeGenerator.cs
@@ -0,0 +1,20 @@
+//Copyright Warren Harding 2025.
+using System;
+
+namespace RulesData.Generators
+{
+    public class IntegerDataTypeGenerator : IDataTypeGenerator
+    {
+        private readonly Random _random;
+
+        public IntegerDataTypeGenerator(Random random)
+        {
+            _random = random;
+        }
+
+        public object Generate()
+        {
+            return _random.Next(-10000, 10000); // Random integer
+        }
+    }
+}
