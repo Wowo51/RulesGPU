ae96fca268389b2e2c232b60c8e06c1b2e9cd203
Warren Harding <warrencharding@yahoo.com>
2025-06-18 00:34:22 -0600

    Add src/RulesDMN/Models/HitPolicy.cs

---
+++ src/RulesDMN/Models/HitPolicy.cs
@@ -0,0 +1,14 @@
+//Copyright Warren Harding 2025.
+namespace RulesDMN.Models
+{
+    public enum HitPolicy
+    {
+        Unique,
+        Any,
+        First,
+        Priority,
+        Collect,
+        RuleOrder,
+        OutputOrder
+    }
+}
