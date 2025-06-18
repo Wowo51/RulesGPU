e32444331d8ad28e984bc208ac1a03fa11e5200a
Warren Harding <warrencharding@yahoo.com>
2025-06-18 00:33:13 -0600

    Add src/RulesGPU/ComparisonOperator.cs

---
+++ src/RulesGPU/ComparisonOperator.cs
@@ -0,0 +1,16 @@
+//Copyright Warren Harding 2025.
+namespace RulesGPU
+{
+    /// <summary>
+    /// Defines the types of comparison operators supported for input conditions.
+    /// </summary>
+    public enum ComparisonOperator : int
+    {
+        Equal = 0,
+        GreaterThan = 1,
+        GreaterThanOrEqual = 2,
+        LessThan = 3,
+        LessThanOrEqual = 4,
+        NotEqual = 5
+    }
+}
