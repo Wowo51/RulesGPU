1850b8db7b01e04799492a87143a3f9b6213ced2
Warren Harding <warrencharding@yahoo.com>
2025-06-18 00:34:32 -0600

    Add src/RulesDMN/Models/LiteralExpression.cs

---
+++ src/RulesDMN/Models/LiteralExpression.cs
@@ -0,0 +1,9 @@
+//Copyright Warren Harding 2025.
+namespace RulesDMN.Models
+{
+    public class LiteralExpression : IExpression
+    {
+        public string Text { get; set; } = string.Empty;
+        public string TypeRef { get; set; } = string.Empty;
+    }
+}
