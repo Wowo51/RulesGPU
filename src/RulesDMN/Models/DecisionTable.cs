43ca8e9386ce07fe02d834861a7ea80e12ae116c
Warren Harding <warrencharding@yahoo.com>
2025-06-18 00:34:16 -0600

    Add src/RulesDMN/Models/DecisionTable.cs

---
+++ src/RulesDMN/Models/DecisionTable.cs
@@ -0,0 +1,20 @@
+//Copyright Warren Harding 2025.
+using System.Collections.Generic;
+
+namespace RulesDMN.Models
+{
+    public class DecisionTable : IExpression
+    {
+        public DecisionTable()
+        {
+            Inputs = new List<InputClause>();
+            Outputs = new List<OutputClause>();
+            Rules = new List<Rule>();
+        }
+
+        public HitPolicy HitPolicy { get; set; }
+        public List<InputClause> Inputs { get; set; }
+        public List<OutputClause> Outputs { get; set; }
+        public List<Rule> Rules { get; set; }
+    }
+}
