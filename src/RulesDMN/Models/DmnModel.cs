5a3590f612a88a072ba273d58c8b85aab986502d
Warren Harding <warrencharding@yahoo.com>
2025-06-18 00:34:19 -0600

    Add src/RulesDMN/Models/DmnModel.cs

---
+++ src/RulesDMN/Models/DmnModel.cs
@@ -0,0 +1,17 @@
+//Copyright Warren Harding 2025.
+using System.Collections.Generic;
+
+namespace RulesDMN.Models
+{
+    public class DmnModel
+    {
+        public DmnModel()
+        {
+            Decisions = new List<Decision>();
+        }
+
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public List<Decision> Decisions { get; set; }
+    }
+}
